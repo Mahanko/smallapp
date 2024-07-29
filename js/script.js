f9ac23df903defaf1df99f7396fd4405ee907122
Mahanko <b9197891075@gmail.com>
2024-07-29 23:22:20 +0330

    78

--- js/script.js
+++ js/script.js
@@ -12,8 +12,18 @@ const addEventOnElements = function (elements, eventType, callback) {
     }
 }
 
-//preload
+/**
+ * PRELOAD
+ * 
+ * loading will be end after document is loaded
+ */
 
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
 
 (function ($) {
   'use strict';
