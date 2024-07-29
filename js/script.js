563ea464d053f5d421387b6dc85dbf9be5cdfa3e
Mahanko <b9197891075@gmail.com>
2024-07-29 23:39:43 +0330

    00open

--- js/script.js
+++ js/script.js
@@ -1,19 +1,7 @@
 // Mahan Ahmadi
 // 2024 
 // ..............................
-
-/**
- * add event listener on multiple elements
- */
-
-const addEventOnElements = function (elements, eventType, callback) {
-    for (let i = 0, len = elements.length; i < len; i++) {
-        elements[i].addEventListener(eventType, callback);
-    }
-}
-
-
-
+// preload 
 (function ($) {
   'use strict';
 
